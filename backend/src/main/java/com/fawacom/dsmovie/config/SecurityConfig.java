9ee5f320bd28b4a19c8ce19c0f91f91ee56302ae
Wagner Rigoli da Rosa <wagner@rigolidarosa.com>
2022-01-12 21:29:54 -0300

    Domain model, database seed

--- backend/src/main/java/com/fawacom/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/fawacom/dsmovie/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.fawacom.dsmovie.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +11,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
